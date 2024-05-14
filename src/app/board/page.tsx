6984c4cbed18316c31de98b7bf60abf24a2635df
Jaweria321 <jaweria.ehsan123@gmail.com>
2024-05-15 01:44:31 +0500

    error fixed

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -19,6 +19,25 @@ function Square({ value, onSquareClick }: squarePropsType) {
     </button>
   );
 }
+function calculateWinner(squares: string[]) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] == squares[b] && squares[a] == squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 function Board({ xIsNext, squares, onPlay }: boardPropsType) {
   function handleClick(i: number) {
     if (calculateWinner(squares) || squares[i]) {
@@ -62,22 +81,4 @@ function Board({ xIsNext, squares, onPlay }: boardPropsType) {
 }
 
 export default Board;
-function calculateWinner(squares: string[]) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] == squares[b] && squares[a] == squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
+
